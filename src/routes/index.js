1cc350f39dbd182ce3b3e4c12f345b673aa86515
commando-famere <commandofamere@gmail.com>
2026-05-21 17:35:14 +0300

    Mount invite routes in main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Import route modules
 const authRoutes = require('./v1/authRoutes');
 const chamaRoutes = require('./v1/chamaRoutes');
+const inviteRoutes = require('./v1/inviteRoutes');
 
 // API v1 routes
 router.use('/auth', authRoutes);
 router.use('/chamas', chamaRoutes);
+router.use('/invites', inviteRoutes);
 
 module.exports = router;
